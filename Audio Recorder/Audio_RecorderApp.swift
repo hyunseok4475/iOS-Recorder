773afa79c4c85192944958b15d7394750abab780
hyunseok4475 <zmfnak135@gmail.com>
2022-01-06 13:04:17 +0900

    Initial Commit

---
+++ Audio Recorder/Audio_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_RecorderApp.swift
+//  Audio Recorder
+//
+//  Created by 송현석 on 2022/01/06.
+//
+
+import SwiftUI
+
+@main
+struct Audio_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
